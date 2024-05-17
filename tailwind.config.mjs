d766d301378f349a4ed637fb3a12f63a8fb5c842
Riandre <ri4ndre@gmail.com>
2024-05-17 12:48:49 +0200

    Updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,37 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				freeman: ['Freeman'],
+				varuna: ['Varuna'],
+				ubuntu: ['Ubuntu'],
+				rubik: ['Rubik'],
+				neuemontreal: ['PP Neue Montreal'],
+			},
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				lg: '0 8px 16px var(--tw-shadow-color)',
+				blur: '0 0 48px var(--tw-shadow-color)',
+				radial: '0 0 0.5rem var(--tw-shadow-color)',
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+			  {
+				'text-shadow': (value) => ({
+				  textShadow: value,
+				}),
+			  },
+			  { values: theme('textShadow') }
+			)
+		  }),
+	],
 }
